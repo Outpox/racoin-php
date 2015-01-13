aad2556c4f34a064f10593c04ef7c0f823ed00ad
Outpox <guillaumeoutpox@gmail.com>
2015-01-13 17:27:55 +0100

    Some changes...

--- controller/index.php
+++ controller/index.php
@@ -2,6 +2,14 @@
 
 namespace controller;
 
+use model\annonce;
+
+include "../db/connection.php";
+
 class index {
-    
+    protected $annonce;
+
+    public function getAll() {
+        
+    }
 }
