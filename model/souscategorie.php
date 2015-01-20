c39fb847355bda904ac8a05a579235e60ae8ddcb
Jacquot12 <david.jacquot88@gmail.com>
2015-01-20 14:40:08 +0100

    corrections d'erreurs mineures

--- model/souscategorie.php
+++ model/souscategorie.php
@@ -2,7 +2,7 @@
 
 namespace model;
 
-class sous_categorie extends \Illuminate\Database\Eloquent\Model {
+class souscategorie extends \Illuminate\Database\Eloquent\Model {
     protected $table = 'sous_categorie';
     protected $id = 'id_sous_categorie';
     public $timestamps = false;
