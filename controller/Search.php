f2ea150f06cf09b387a54f77f1232519db46a76d
Jacquot12 <david.jacquot88@gmail.com>
2015-01-27 16:41:33 +0100

    search function op (little changes incoming (CP))

--- controller/Search.php
+++ controller/Search.php
@@ -13,17 +13,10 @@ class Search {
     }
 
     function research($array, $twig, $menu, $chemin) {
-        //print_r($array);
-        $template = $twig->loadTemplate("searchresult.html.twig");
+        $template = $twig->loadTemplate("index.html.twig");
 
         $nospace_mc = str_replace(' ', '', $array['motclef']);
         $nospace_cp = str_replace(' ', '', $array['codepostal']);
-
-        /*echo "mc : ".($nospace_mc === "")."</br>";
-        echo "cp : ".($nospace_cp === "")."</br>";
-        echo "categ : ".($array['categorie'] === "Toutes catégories" || $array['categorie'] === "-----")."</br>";
-        echo "p min : ".($array['prix-min'] === "Min")."</br>";
-        echo "p max : ".($array['prix-max'] === "Max")."</br>";*/
 
         $ltannonce = array();
         // Si tout est vide on affiche tout
@@ -34,13 +27,9 @@ class Search {
 
             foreach(Annonce::all() as $annonce) {
                 array_push($ltannonce, $annonce);
-                //$ltannonce .= $annonce;
             }
             //affiche tout
         } else {
-            // Affiche résultat de la recherche
-            $recherche = array();
-            $nb = 0;
             foreach(Annonce::all() as $annonce) {
                 // Test si annonce est dans la bonne tranche de prix
                 if( (($annonce->prix >= $array['prix-min']) && ($annonce->prix <= $array['prix-max'])) ||
@@ -58,27 +47,18 @@ class Search {
                         if( (($title !== false) || ($desc !== false)) || ($nospace_mc === "") ) {
 
                             // Test si l'annonce est dans la bonne categorie
-                            $sscateg = SousCategorie::find($annonce->id_sous_categorie); // $array['categorie']
+                            $sscateg = SousCategorie::find($annonce->id_sous_categorie);
                             if ( ($sscateg->id_categorie === $array['categorie']) || (($array['categorie'] === "Toutes catégories" || $array['categorie'] === "-----")) ) {
-                                //$ltannonce .= $annonce;
-                                //$recherche[$nb] = $annonce;
-                                //$nb++;
+                                array_push($ltannonce, $annonce);
                             }
                         }
 
                     }
                 }
-                //echo $recherche[$nb]."<br/><br/>";
-            }
-
-            foreach($recherche as $value) {
-                // afficher la recherche
             }
         }
 
-        //echo $ltannonce[0];
-        //echo $template->render(array("breadcrumb" => $menu, "chemin" => $chemin));
-        //$template->display( array('liste_annonce' => $ltannonce));
+        echo $template->render(array("breadcrumb" => $menu, "chemin" => $chemin, "annonces" => $ltannonce));
     }
 }
 
