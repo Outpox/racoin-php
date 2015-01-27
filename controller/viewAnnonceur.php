58ddc5cb1b248d08561fdc0bc3bbc2a6249636ed
Ponicorn <tougrer@gmail.com>
2015-01-27 11:32:14 +0100

    view annonceur fully functionnal

--- controller/viewAnnonceur.php
+++ controller/viewAnnonceur.php
@@ -9,19 +9,31 @@
 namespace controller;
 use model\annonce;
 use model\annonceur;
+use model\photo;
 
 class viewAnnonceur {
     public function __construct(){
     }
     function afficherAnnonceur($twig, $menu, $chemin,$n) {
         $this->annonceur = annonceur::find($n);
-//        $this->annonces = annonce::where('id_annonceur','=',$n)->get();
-        $this->annonces = annonce::join('photo','photo.id_annonce','=','annonce.id_annonce')
-                ->where('annonce.id_annonceur','=',$n)
-                ->get();
+        $tmp = annonce::where('id_annonceur','=',$n)->get();
+
+        $annonces = [];
+        foreach ($tmp as $a) {
+            $a->nb_photo = Photo::where('id_annonce', '=', $a->id_annonce)->count();
+            if($a->nb_photo>0){
+                $a->url_photo = Photo::select('url_photo')
+                    ->where('id_annonce', '=', $a->id_annonce)
+                    ->first()->url_photo;
+            }else{
+                $a->url_photo = $chemin.'/img/canon5d.png';
+            }
+
+            $annonces[] = $a;
+        }
         $template = $twig->loadTemplate("annonceur.html.twig");
         echo $template->render(array('nom' => $this->annonceur,
             "chemin" => $chemin,
-            "annonces" => $this->annonces));
+            "annonces" => $annonces));
     }
 }
