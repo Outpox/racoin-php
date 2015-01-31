c737da21b4c213acbb47e94cdc833f803b284d39
Nicolas Obara <nicolas.obara@gmail.com>
2015-01-31 16:48:17 +0100

    updated l'ajout

--- controller/addItem.php
+++ controller/addItem.php
@@ -7,10 +7,15 @@ use model\Annonceur;
 
 class addItem{
 
-    function addItemView($twig, $menu, $chemin){
+    function addItemView($twig, $menu, $chemin, $cat, $dpt){
 
         $template = $twig->loadTemplate("add.html.twig");
-        echo $template->render(array("breadcrumb" => $menu, "chemin" => $chemin));
+        echo $template->render(array(
+                                    "breadcrumb" => $menu,
+                                    "chemin" => $chemin,
+                                    "categories" => $cat,
+                                    "departements" => $dpt)
+                                );
 
     }
 
@@ -23,10 +28,15 @@ class addItem{
             return(preg_match("/^[-_.[:alnum:]]+@((([[:alnum:]]|[[:alnum:]][[:alnum:]-]*[[:alnum:]])\.)+(ad|ae|aero|af|ag|ai|al|am|an|ao|aq|ar|arpa|as|at|au|aw|az|ba|bb|bd|be|bf|bg|bh|bi|biz|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|com|coop|cr|cs|cu|cv|cx|cy|cz|de|dj|dk|dm|do|dz|ec|edu|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gh|gi|gl|gm|gn|gov|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|in|info|int|io|iq|ir|is|it|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|mg|mh|mil|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|museum|mv|mw|mx|my|mz|na|name|nc|ne|net|nf|ng|ni|nl|no|np|nr|nt|nu|nz|om|org|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|pro|ps|pt|pw|py|qa|re|ro|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|sk|sl|sm|sn|so|sr|st|su|sv|sy|sz|tc|td|tf|tg|th|tj|tk|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|um|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)$|(([0-9][0-9]?|[0-1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5])\.){3}([0-9][0-9]?|[0-1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5]))$/i", $email));
         }
 
+        /*
+        * On récupère tous les champs du formulaire en supprimant
+        * les caractères invisibles en début et fin de chaîne.
+        */
         $nom = trim($_POST['nom']);
         $email = trim($_POST['email']);
         $phone = trim($_POST['phone']);
         $ville = trim($_POST['ville']);
+        $departement = trim($_POST['departement']);
         $categorie = trim($_POST['categorie']);
         $title = trim($_POST['title']);
         $description = trim($_POST['description']);
@@ -34,17 +44,20 @@ class addItem{
         $password = trim($_POST['psw']);
         $password_confirm = trim($_POST['confirm-psw']);
 
+        // Tableau d'erreurs personnalisées
         $errors = array();
         $errors['nameAdvertiser'] = '';
         $errors['emailAdvertiser'] = '';
         $errors['phoneAdvertiser'] = '';
         $errors['villeAdvertiser'] = '';
+        $errors['departmentAdvertiser'] = '';
         $errors['categorieAdvertiser'] = '';
         $errors['titleAdvertiser'] = '';
         $errors['descriptionAdvertiser'] = '';
         $errors['priceAdvertiser'] = '';
         $errors['passwordAdvertiser'] = '';
 
+        // On teste que les champs ne soient pas vides et soient de bons types
         if(empty($nom)) {
             $errors['nameAdvertiser'] = 'Veuillez entrer votre nom';
         }
@@ -57,7 +70,10 @@ class addItem{
         if(empty($ville)) {
             $errors['villeAdvertiser'] = 'Veuillez entrer votre ville';
         }
-        if(empty($categorie)) {
+        if(!is_numeric($departement)) {
+            $errors['departmentAdvertiser'] = 'Veuillez choisir un département';
+        }
+        if(!is_numeric($categorie)) {
             $errors['categorieAdvertiser'] = 'Veuillez choisir une catégorie';
         }
         if(empty($title)) {
@@ -66,26 +82,27 @@ class addItem{
         if(empty($description)) {
             $errors['descriptionAdvertiser'] = 'Veuillez entrer une description';
         }
-        if(empty($price)) {
+        if(empty($price) || !is_numeric($price)) {
             $errors['priceAdvertiser'] = 'Veuillez entrer un prix';
         }
         if(empty($password) || empty($password_confirm) || $password != $password_confirm) {
-            $errors['priceAdvertiser'] = 'Les mots de passes ne sont pas identiques';
+            $errors['passwordAdvertiser'] = 'Les mots de passes ne sont pas identiques';
         }
 
+        // On vire les cases vides
+        $errors = array_values(array_filter($errors));
+
+        // S'il y a des erreurs on redirige vers la page d'erreur
         if (!empty($errors)) {
 
-            echo '<ul>'."\n";
-
-            foreach($errors as $e) {
-
-                if (!empty($e)){
-                    echo '<li>'.$e.'</li>'."\n";
-                }
-            }
-
-            echo '</ul>';
+            $template = $twig->loadTemplate("add-error.html.twig");
+            echo $template->render(array(
+                                    "breadcrumb" => $menu,
+                                    "chemin" => $chemin,
+                                    "errors" => $errors)
+                                );
         }
+        // sinon on ajoute à la base et on redirige vers une page de succès
         else{
             $annonce = new Annonce();
             $annonceur = new Annonceur();
@@ -94,6 +111,8 @@ class addItem{
             $annonceur->nom_annonceur = htmlentities($allPostVars['nom']);
             $annonceur->telephone = htmlentities($allPostVars['phone']);
 
+            $annonce->ville = htmlentities($allPostVars['ville']);
+            $annonce->id_departement = htmlentities($allPostVars['departement']);
             $annonce->prix = htmlentities($allPostVars['price']);
             $annonce->mdp = password_hash ($allPostVars['psw'], PASSWORD_DEFAULT);
             $annonce->titre = htmlentities($allPostVars['title']);
@@ -102,7 +121,7 @@ class addItem{
             $annonce->date = date('Y-m-d');
 
             $annonceur->save();
-            $annonceur->annonce()->save($annonce);
+            $annonceur->Annonce()->save($annonce);
 
             $template = $twig->loadTemplate("add-confirm.html.twig");
             echo $template->render(array("breadcrumb" => $menu, "chemin" => $chemin));
