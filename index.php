1038dbf84c070f13af1bee711f82a0dedd4c41e7
Jacquot12 <david.jacquot88@gmail.com>
2015-02-03 16:05:06 +0100

    start token

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
 <?php
 require 'vendor/autoload.php';
 use db\connection;
+use Slim\Extras\Middleware\CsrfGuard;
 connection::createConn();
 
 
 $app = new \Slim\Slim(array(
     'mode' => 'development'
 ));
+
+session_start();
+$app->add(new CsrfGuard());
 
 $loader = new Twig_Loader_Filesystem('template');
 $twig = new Twig_Environment($loader);
